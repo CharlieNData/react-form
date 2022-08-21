666b53f4f4ddbd860dc61c769c283963598dad64
CharlieNData <charlesejewers@gmail.com>
2022-08-21 22:56:45 +0100

    State for text fields and Success button

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
 import './App.css';
-import { useState } from 'react';
+import React, {useState} from "react";
 
 function App() {
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  const handleFirstNameInputChange = (event) => {
+    setValues({...values, firstName: event.target.value})
+  }
+
+  const handleLastNameInputChange = (event) => {
+    setValues({...values, lastName: event.target.value})
+  }
+
+  const handleEmailInputChange = (event) => {
+    setValues({...values, email: event.target.value})
+  }
 
   return (
     <div className="form-container">
       <form className='register-form'>
-        <input 
+        <div className='success-message'>Success! You have registered.</div>
+        <input
+          onChange={handleFirstNameInputChange}
+          value={values.firstName} 
           className='form-field'
+          type='text'
           placeholder='Name'
           name='firstName'
         />
-        <input 
+        <input
+          onChange={handleLastNameInputChange} 
+          value={values.lastName}
           className='form-field'
+          type='text'
+          placeholder='Last Name'
+          name='lastname'
+        />
+        <input
+          onChange={handleEmailInputChange} 
+          className='form-field'
+          type='text'
           placeholder='Email'
           name='email'
         />
-        <input 
-          className='form-field'
-          placeholder='Phone Number'
-          name='phoneNumber'
-        />
         <button
+          value={values.email}
           className='form-field button'
+          type='submit'
         >Submit</button>
       </form>
     </div>
